eaecbc81bfe9dab1f57ffee5b4d0fd1cb21373b0
Yuri Thairony <yurithairony@hotmail.com>
2023-09-12 17:59:53 -0300

    Recriando os métodos da estruturas lineares

--- avl/avl.cs
+++ avl/avl.cs
@@ -1,8 +1,180 @@
-
+using System.Collections;
 
 public class Avl {
+    private Node root;
+    private int length = 0;
+    private ArrayList showTree;
 
-}
+
+    // Construtor
+    public Avl(object elem) {
+        this.root = new Node(null, elem);
+        this.length++;
+    }
+
+
+    //Métodos básicos de verificação
+    public int Length() {
+        return length;
+    }
+
+
+    public bool IsEmpty() {
+        return root == null;   
+    }
+
+
+    public Node Root() {
+        return root;
+    }
+
+
+    public bool IsRoot(Node node) {
+        return root == node;
+    }
+
+
+    public static bool IsInternal(Node node) {
+        return node.GetLeftChild() != null || node.GetRightChild() != null;
+    }
+
+
+    public static bool IsExternal(Node node) {
+        return node.GetLeftChild() == null && node.GetRightChild() == null;
+    }
+
+
+    //Métodos de comparação
+    private static bool IsLeft(Node node) {
+        return node.GetLeftChild() == null;
+    }
+    
+
+    private static bool IsRight(Node node) {
+        return node.GetRightChild() == null;
+    }
+
+
+    //Método de pesquisa
+    private Node SearchInt(Node node, object elem) {
+        if ((int)elem < (int)node.GetElem()) {
+            if (IsLeft(node)) {
+                return node;
+            } else {
+                return SearchInt(node.GetLeftChild(), elem);
+            }
+        } else if((int)elem > (int)node.GetElem()) {
+            if (IsRight(node)) {
+                return node;
+            } else {
+                return SearchInt(node.GetRightChild(), elem);
+            }
+        } else {
+            return node;
+        }
+    }
+
+    //Método de inserção
+    public Node Push(object elem) {
+        Node dad = SearchInt(root, elem);
+        Node newNode = new Node(dad, elem);
+
+        if((int)elem < (int)dad.GetElem()) {
+            dad.SetLeftChild(newNode);
+        } else {
+            dad.SetRightChild(newNode);
+        }
+
+        length++;
+        return newNode;
+    }
+
+
+    //Método de remoção
+
+
+    // Métodos de ordenação Sucessão, EmOrdem, PréOrdem e PosOrdem
+    private static Node Sucessor(Node node) {
+        Node sucessor = node.GetRightChild();
+        while(sucessor.GetLeftChild() != null) {
+            sucessor = sucessor.GetLeftChild();
+        }
+
+        return sucessor;
+    }
+
+
+    private void InOrder(Node node) {
+        if(IsInternal(node)) {
+            if(!IsLeft(node)) {
+                InOrder(node.GetLeftChild());
+            }
+        } 
+
+        showTree.Add(node.GetElem());
+
+        if(IsInternal(node)) {
+            if(!IsRight(node)) {
+                InOrder(node.GetRightChild());
+            }
+        }
+    }
+
+
+    private void PreOrder(Node node) {
+        showTree.Add(node.GetElem());
+
+        if(IsInternal(node)) {
+            if(!IsLeft(node)) {
+                InOrder(node.GetLeftChild());
+            }
+        } 
+
+        if(IsInternal(node)) {
+            if(!IsRight(node)) {
+                InOrder(node.GetRightChild());
+            }
+        }
+    }
+
+
+    private void PosOrder(Node node) {
+        if(IsInternal(node)) {
+            if(!IsLeft(node)) {
+                InOrder(node.GetLeftChild());
+            }
+        } 
+
+        if(IsInternal(node)) {
+            if(!IsRight(node)) {
+                InOrder(node.GetRightChild());
+            }
+        }
+
+        showTree.Add(node.GetElem());
+    }
+
+
+    public IEnumerator Nodes() {
+        showTree = new ArrayList();
+        PreOrder(root);
+        return showTree.GetEnumerator();
+    }
+
+
+    public IEnumerator Elements() {
+        showTree = new ArrayList();
+        InOrder(root);
+        return showTree.GetEnumerator();
+    }
+
+
+    //Métodos de altura e profundidade
+
+
+ }
+
+
 
 public class Node {
     private Node dad;
@@ -18,7 +190,7 @@ public class Node {
 
 
     public object GetElem() {
-        return this.elem;
+        return elem;
     }
 
 
@@ -34,6 +206,11 @@ public class Node {
 
     public Node GetRightChild() {
         return rightChild;
+    }
+
+
+    public void SetElem(object elem) {
+        this.elem = elem;
     } 
 
 
